5722f5417b064e96a5f4834ca9b41a45dc915501
yyang11 <lovingcode@gmail.com>
2014-10-22 09:18:32 +0800

    Update ValidateBST.java

--- src/done/ValidateBST.java
+++ src/done/ValidateBST.java
@@ -2,53 +2,41 @@ import done.TreeNode;
 
 import java.util.Stack;
 
+//classic
 public class ValidateBST {
-  //non-recursive
-  public boolean isValidBST(TreeNode root) {
-    Stack<TreeNode> stack = new Stack<TreeNode>();
-    TreeNode node = root;
-    int prev = Integer.MIN_VALUE;
-    while(node != null || !stack.isEmpty()) {
-      while(node != null) {
-        stack.push(node);
-        node = node.left;
-      }
-      node = stack.pop();
-      if(prev >= node.val)
-        return false;
-      prev = node.val;
-      node = node.right;
-    }
-    return true;
-  }
-
+  //iterative
 //  public boolean isValidBST(TreeNode root) {
-//    if(root == null)
-//      return true;
-//    int[] ret = new int[2];
-//    return isValidBST(root, ret);
-//  }
-//
-//  boolean isValidBST(TreeNode root, int[] ret) {
-//    int min = root.val, max = root.val;
-//    if(root.left != null) {
-//      if(!isValidBST(root.left, ret) || ret[1] >= root.val)
+//    Stack<TreeNode> stack = new Stack<TreeNode>();
+//    TreeNode node = root;
+//    int prev = Integer.MIN_VALUE;
+//    while(node != null || !stack.isEmpty()) {
+//      while(node != null) {
+//        stack.push(node);
+//        node = node.left;
+//      }
+//      node = stack.pop();
+//      if(prev >= node.val)
 //        return false;
-//      min = ret[0];
+//      prev = node.val;
+//      node = node.right;
 //    }
-//    if(root.right != null) {
-//      if(!isValidBST(root.right, ret) || ret[0] <= root.val)
-//        return false;
-//      max = ret[1];
-//    }
-//    ret[0] = min;
-//    ret[1] = max;
 //    return true;
 //  }
 
-    public static void main(String args[]) {
-    TreeNode root = new TreeNode(2);
-    root.left = new TreeNode(1);
+  //revursive
+  int prev = Integer.MIN_VALUE;
+  public boolean isValidBST(TreeNode root) {
+    if(root == null)
+      return true;
+    if(!isValidBST(root.left) || prev >= root.val)
+      return false;
+    prev = root.val;
+    return isValidBST(root.right);
+  }
+
+  public static void main(String args[]) {
+    TreeNode root = new TreeNode(1);
+    root.left = new TreeNode(2);
     root.right = new TreeNode(3);
     boolean result = new ValidateBST().isValidBST(root);
     System.out.println(result);
