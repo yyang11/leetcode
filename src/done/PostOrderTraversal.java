92bdeac2ebb5301ad02328e0e73b58b27ad95965
yyang11 <lovingcode@gmail.com>
2014-10-23 14:46:40 +0800

    Create PostOrderTraversal.java

---
+++ src/done/PostOrderTraversal.java
@@ -0,0 +1,21 @@
+import javax.swing.tree.TreeNode;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+public class Postorder {
+  public List<Integer> postorderTraversal(TreeNode root) {
+    Stack<TreeNode> stack = new Stack<TreeNode>();
+    LinkedList<Integer> result = new LinkedList<Integer>();
+    stack.push(root);
+    while(root != null && !stack.isEmpty()) {
+      root = stack.pop();
+      result.addFirst(root.val);
+      if(root.left != null)
+        stack.push(root.left);
+      if(root.right != null)
+        stack.push(root.right);
+    }
+    return result;
+  }
+}
