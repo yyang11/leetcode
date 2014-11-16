b191b8531f4df8dabbcbde8304f92bce86b38ff3
yyang11 <lovingcode@gmail.com>
2014-11-16 10:52:07 +0800

    Update LargestRectangleInHistogram.java

--- src/done/LargestRectangleInHistogram.java
+++ src/done/LargestRectangleInHistogram.java
@@ -1,18 +1,21 @@
+import java.util.Arrays;
+import java.util.Stack;
+
 public class LargestRectangleInHistogram {
   public int largestRectangleArea(int[] height) {
-      Stack<Integer> stack = new Stack<Integer>();
-            int i = 0;
-             int maxArea = 0;
-             int[] h = new int[height.length + 1];
-             h = Arrays.copyOf(height, height.length + 1);
-             while(i < h.length){
-                 if(stack.isEmpty() || h[stack.peek()] <= h[i]){
-                      stack.push(i++);
-                    }else {
-                         int t = stack.pop();
-                         maxArea = Math.max(maxArea, h[t] * (stack.isEmpty() ? i : i - stack.peek() - 1));
-                     }
-              }
-             return maxArea;
+    Stack<Integer> stack = new Stack<Integer>();
+    int i = 0;
+    int maxArea = 0;
+    int[] h = new int[height.length + 1];
+    h = Arrays.copyOf(height, height.length + 1);
+    while (i < h.length) {
+      if (stack.isEmpty() || h[stack.peek()] <= h[i]) {
+        stack.push(i++);
+      } else {
+        int t = stack.pop();
+        maxArea = Math.max(maxArea, h[t] * (stack.isEmpty() ? i : i - stack.peek() - 1));
+      }
+    }
+    return maxArea;
   }
 }
