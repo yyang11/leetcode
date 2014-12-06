0f7cbb91831847d849b4cfe908d3d80b4f4ab7a2
yyang11 <602981061@qq.com>
2014-12-06 16:19:02 +0800

    Update Pow.java

--- src/done/Pow.java
+++ src/done/Pow.java
@@ -9,4 +9,20 @@ public class Pow {
         double half=pow(x,n/2);
         return half*half*extra;
     }
+    
+    /*
+    public double pow(double x, int n) {
+        //non-recursive
+        //awesome!
+        if(n<0) return 1/(x*pow(x,-(n+1)));
+        if(n==0) return 1;
+        double left=x,right=1;
+        while(n!=1) {
+            if(n%2==1) right*=left;
+            left*=left;
+            n/=2;
+        }
+        return left*right;
+    }
+    */
 }
